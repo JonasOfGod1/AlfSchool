225af4779a768de65d1de208fcdf9b22a039ab05
jonas de Deus <jonasmdd012@gmail.com>
2021-11-11 20:14:58 -0300

    Atualizações 11/11/2021 - 20:15

---
+++ Program/DbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Program
+{
+    public class DbContext
+    {
+
+    }
+}
--- Program/Telas/Administrador/CadastrarAluno.cs
+++ Program/Telas/Administrador/CadastrarAluno.cs
@@ -1,16 +1,71 @@
-﻿using Program.Models.Aluno;
+﻿using MySqlConnector;
+using Program.Models.Aluno;
 using Program.Services;
 using Program.Telas.Administrador;
 using System;
+using System.Data;
 using System.Windows.Forms;
 
 namespace Program.Telas
 {
     public partial class CadastrarAluno : Form
     {
+        
+        private MySqlConnection mConn;
+        private MySqlDataAdapter mAdapter;
+        private DataSet mDataSet;
+
         public CadastrarAluno()
         {
             InitializeComponent();
+            
+
+                mDataSet = new DataSet();
+
+
+            //define string de conexao e cria a conexao
+
+            mConn = new MySqlConnection(" Persist Security Info=False;server=localhost;database=Cadastro;uid=root;server = localhost; database = Cadastro; uid = root; pwd = xxxx");
+
+
+      try
+            {
+
+                //abre a conexao
+
+                mConn.Open();
+
+            }
+
+            catch (System.Exception e)
+
+            {
+
+                MessageBox.Show(e.Message.ToString());
+
+            }
+
+
+
+            //verificva se a conexão esta aberta
+
+            if (mConn.State == ConnectionState.Open)
+
+            {
+
+                //cria um adapter usando a instrução SQL para acessar a tabela Clientes
+
+                mAdapter = new MySqlDataAdapter("SELECT * FROM Clientes", mConn);
+
+                //preenche o dataset via adapter
+
+                mAdapter.Fill(mDataSet, "Clientes");
+
+                //atribui a resultado a propriedade DataSource do DataGrid
+
+
+            }
+
         }
 
         private void btnCadastrarAluno_Click(object sender, EventArgs e)
